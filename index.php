b2b147fef4307a670efaf2c720f75bf21d01e802
csorbakristof <csorba.kristof@vik.bme.hu>
2023-02-28 10:37:41 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Telefonkönyv</title>
+       <link rel="stylesheet" type="text/css" href="theme.css">   
+    </head>
+    <body>
+        
+        <h1>Telefonkönyv</h1>
+        <table>
+            <tr>
+                <th>Név</th>
+                <th>Telefon</th>      
+            </tr> 
+        </table>
+        <p>
+           <a href="insert.php">Új elem beszúrása</a>
+        </p>
+    </body>
+</html>
---
+++ insert.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head><title>Telefonkönyv</title></head>
+    <body>
+        <form action="insert.php" method="post">
+            <h1>Új telefonszám</h1>
+            <p>
+                Név: <input type="text" name="nev" />    
+            </p>
+            <p>
+                Telefonszám: <input type="text" name="szam" />    
+            </p>
+            <p> 
+                <input type="submit" value="Elküld" name="uj" />
+            </p>
+        </form>
+    </body>
+</html>
+
